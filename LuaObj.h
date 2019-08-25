fb4495f639d2b6b657ca5422bcfee29f9c6a9252
罗浩恒 <lhh0461@175game.com>
2019-08-26 00:23:14 +0800

    去掉一些文件

--- LuaObj.h
+++
@@ -1,5 +0,0 @@
-#define RPC_OBJ PyObject
-#define RPC_INT PyObject
-#define RPC__OBJ PyObject
-#define RPC_TABLE_OBJ PyObject
-#define RPC_TABLE_OBJ PyObject
--- PythonObj.h
+++
@@ -1,5 +0,0 @@
-#define RPC_OBJ PyObject
-#define RPC_INT PyLong_FromLong
-#define RPC_OBJ PyObject
-#define RPC_TABLE_OBJ PyObject
-#define RPC_TABLE_OBJ PyObject
