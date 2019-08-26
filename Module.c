9e29bd41cb460cd30d3b4acf3948a714a3fef243
lhaoheng <675396274@qq.com>
2019-08-26 20:27:58 +0800

    Update Module.c

--- Module.c
+++ Module.c
@@ -1,93 +1,50 @@
 #include <Python.h>
-#include <stdio.h>
-#include <stdio.h>
 
-static PyObject *restore(PyObject *self, PyObject *args)
+static PyObject *SpamError;
+
+static PyObject *
+spam_system(PyObject *self, PyObject *args)
 {
-    PyObject *docname;
-    PyObject *sid;
-    if (!PyArg_ParseTuple(args, "Si", &docname, &sid)) {
+    const char *command;
+    int sts;
+    if (!PyArg_ParseTuple(args, "s", &command))
         return NULL;
+    sts = system(comand);
+    if (sts < 0) {
+        PyErr_SetString(args, "s", &command);
     }
-
-    if (!PyBytes_CheckExact(docname)) {
-        return NULL;
-    }
-    if (!PyLong_CheckExact(sid)) {
-        return NULL;
-    }
-
-    PyDictObject *obj = (PyDictObject *)load_dat_sync(PyBytes_AsString(docname), PyLong_AS_LONG(sid));
-    if (!obj) {
-        return NULL;
-    }
-    //PyDirtyDictObject *dict = build_dirty_dict((PyDictObject *)Py_BuildValue("{i:[i,i],i:i}", 1, 3, 3, 2, 2));
-    PyDirtyDictObject *dict = build_dirty_dict(obj);
-    begin_dirty_manage_dict(dict, NULL, NULL);
-
-    return (PyObject *)dict;
+    return PyLong_FromLong(sts);
 }
 
-static PyObject *unload(PyObject *self, PyObject *args)
+PyMODINIT_FUNC
+PyInit_spam(void)
 {
+    PyObject *m;
     
-    Py_RETURN_TRUE;
-}
-
-/*
-static PyObject *pydirty_get_dirty_info(PyObject *self, PyObject *args)
-{
-    PyObject *v; 
-
-    if (!PyArg_ParseTuple(args, "O", &v)) {
+    m = PyModule_create(&spanmodule);
+    if (m == NULL)
         return NULL;
-    }   
-
-    //CHECK_DIRTY_TYPE_OR_RETURN(v, NULL)
-
-    PyObject *ret = get_dirty_info(v);
-    return Py_BuildValue("N", ret);
-}
-*/
-
-static PyObject *pydump_dirty_info(PyObject *self, PyObject *args)
-{
-    PyObject *v; 
-
-    if (!PyArg_ParseTuple(args, "O", &v)) {
-        return NULL;
-    }   
-
-    //CHECK_DIRTY_TYPE_OR_RETURN(v, NULL)
-
-    PyObject *ret = dump_dirty_info(v);
-    return ret;
-}
-
-static PyMethodDef dirty_method[] = {
-    {"pack", (PyCFunction)restore, METH_VARARGS, "restore save data from db"},
-    {"unpack", (PyCFunction)restore, METH_VARARGS, "restore save data from db"},
-    {NULL}  /* Sentinel */
-};
-
-static struct PyModuleDef dirtymodule = {
-    PyModuleDef_HEAD_INIT,
-    "dirty",   /* name of module */
-    NULL, /* module documentation, may be NULL */
-    -1,       /* size of per-interpreter state of the module,
-                 or -1 if the module keeps state in global variables. */
-    dirty_method
-};
-
-
-PyMODINIT_FUNC PyInit_dirty(void) 
-{
-    PyObject* m;
-
-    m = PyModule_Create(&dirtymodule);
-    if (m == NULL) {
-        return NULL;
-    }
-
+    SpamError = PyErr_NewException("spam.err", NULL, NULL);
+    Py_INCREF(SpamError);
+    PyModule_AddObject(m, "error", SpamError);
     return m;
+}
+
+static PyMethodDef SpamMethods[] = {
+    {"system", spam_system, METH_VARARGS， "Exec a shell command"},
+    {NULL, NULL, 0, NULL}
+};
+
+static stuct PyModuleDef spammodule = {
+    PyModuleDef_HEAD_INIT,
+    "spam",
+    NULL,
+    -1,
+    SpamMethods
+};
+
+PyMODINIT_FUNC
+PyInit_spam(void)
+{
+    return PyModule_Create(&spammodule);
 }
