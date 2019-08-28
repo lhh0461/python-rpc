e86a183ebc8aa5b95c65510ccb87594ddc36bf5a
lhaoheng <675396274@qq.com>
2019-08-28 14:11:54 +0800

    Update Module.cpp

--- Module.cpp
+++ Module.cpp
@@ -48,9 +48,33 @@ static PyObject *unpack(PyObject *self, PyObject *args)
     return unpackObj;
 }
 
+static PyObject *rpc_call(PyObject *self, PyObject *args)
+{
+    PyObject *obj;
+
+    if (!PyArg_ParseTuple(args, "O", &obj))
+        return NULL;
+    
+    if (!PyBytes_CheckExact(obj)) {
+        PyErr_Format(PyExc_TypeError, "pack obj type error type=%s", Py_TYPE(obj)->tp_name);
+        return NULL;
+    }
+
+    const char *str = PyBytes_AsString(obj);
+    Py_ssize_t size = PyBytes_Size(obj);
+
+    PyObject *unpackObj = g_Rpc->UnPack(str, size);
+    if (unpackObj == NULL) {
+        PyErr_SetString(RpcError, "Rpc unpack fail");
+        return NULL;
+    }
+    return unpackObj;
+}
+
 static PyMethodDef RpcMethods[] = {
     {"pack", pack, METH_VARARGS, "rpc pack to buf"},
-    {"unpack", unpack, METH_VARARGS, "Rpc unpack from buf"},
+    {"unpack", unpack, METH_VARARGS, "rpc unpack from buf"},
+    {"rpc_call", unpack, METH_VARARGS, "rpc unpack from buf"},
     {NULL, NULL, 0, NULL}
 };
 
